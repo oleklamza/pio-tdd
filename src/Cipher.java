8b917d70f1e7edc2a759c98dd0b798d2c93a4a2d
olekl <olek.lamza@gmail.com>
2024-01-14 22:34:28 +0100

    Pusty projekt przygotowany na TDD

--- src/Cipher.java
+++ src/Cipher.java
@@ -1,18 +1,4 @@
 
 public class Cipher {
 
-    public String encrypt(String in) {
-        String out = "";
-        for (int i=0; i<in.length(); ++i) {
-            char c = in.charAt(i);
-            c += 1;
-            // najprostsza implementacja "zawinięcia" na końcu alfabetu
-            if (c == '[') {
-                c = 'A';
-            }
-            out += c;
-        }
-        return out;
-    }
-
 }
